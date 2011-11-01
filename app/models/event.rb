48490de02e672ee5fa8a33a9ac7d687142a9fa60
johnwang <djmnemonic@gmail.com>
2011-11-01 15:39:56 -0400

    Fixed event.rb :through error.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ActiveRecord::Base
   has_many :users, :through => :user_events
   
   has_many :idea_events
-  has_one :idea, through => :idea_events
+  has_one :idea, :through => :idea_events
   
   has_attached_file :photo, :styles => { :medium => "300x300#", :thumb => "100x100#", :stream => "50x50#" }
 
