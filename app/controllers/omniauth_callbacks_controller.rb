898ab6a55530c10e9c725be16c8f6a575b2194ad
John Wang <djmnemonic@gmail.com>
2011-11-16 22:29:25 -0500

    Changed omniauth.auth access call to originate from request object per documentation.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,9 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user, "facebook")
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user, "facebook")
   
     flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-    session["devise.facebook_data"] = env["omniauth.auth"]
+    session["devise.facebook_data"] = request.env["omniauth.auth"]
     
     sign_in(@user)
 #    render 'users/facebook_callback'
